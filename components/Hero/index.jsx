8310fbc64ccaf27f0905852622a3a2498ae66410
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-04-02 14:05:52 +0200

    Traduction index.jsx
    Traduction Fr de la page principale Home (components/Hero/index.jsx).

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -15,23 +15,23 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and Open-Source Next.js Template for Startup & SaaS
+                Modèle pour Startup SaaS Open Source & en Next.js
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Startup is free Next.js template for startups and SaaS business websites comes with all the essential pages, components, and sections you need to launch a complete business website, built-with Next 13.x and Tailwind CSS.
+                Ce Modèle Startup en Next.js pour les startups et les sites Web d&apos;entreprise SaaS est livré avec toutes les pages, composants et sections essentiels dont vous avez besoin pour lancer un site Web d&apos;entreprise complet, intégré avec Next 13&#46;x et Tailwind CSS.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/startup"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    Download Now
+                    Télécharger
                   </Link>
                   <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"
                     className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
                   >
-                    Star on GitHub
+                    Étoile sur GitHub
                   </Link>
                 </div>
               </div>
