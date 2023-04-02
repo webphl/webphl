b1b82feddf35f69ee40b412ba08e6ed02233f083
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-04-02 14:31:25 +0200

    Traduction Fr de la partie caractéristiques
    Traduction en Fr de la partie caractéristiques principale (components/Features/index.jsx).

--- components/Features/index.jsx
+++ components/Features/index.jsx
@@ -11,8 +11,8 @@ const Features = () => {
       >
         <div className="container">
           <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            title="Caractéristiques Principales"
+            paragraph="Voici les principales caractéristiques de ce modèle de page Startup SaaS en Nextjs."
             center
           />
 
