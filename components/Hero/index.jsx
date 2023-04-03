81184fec261dfbd611528789733e20907fc1cf77
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-04-03 19:46:41 +0200

    Update index.jsx

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -22,13 +22,13 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/startup"
+                    href="https://github.com/webphl/webphl"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Télécharger
                   </Link>
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    href="https://github.com/webphl/webphl"
                     className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
                   >
                     Étoile sur GitHub
