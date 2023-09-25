bd661a5cc69d3722a0d2777ae934c9054159103f
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-09-25 17:00:34 +0200

    Update page.jsx

--- app/blog-sidebar/page.jsx
+++ app/blog-sidebar/page.jsx
@@ -13,8 +13,8 @@ const BlogSidebarPage = () => {
             <div className="w-full px-4 lg:w-8/12">
               <div>
                 <h2 className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight">
-                  10 amazing sites to download stock photos & digital assets for
-                  free
+                  10 sites incroyables pour télécharger des photos et des actifs numériques
+gratuits.
                 </h2>
                 <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
                   <div className="flex flex-wrap items-center">
