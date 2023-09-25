76c82bc6ad004ba27f723597b54ee112ced91b85
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-09-25 16:30:53 +0200

    Update index.jsx

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -54,7 +54,7 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="/images/logo/logo.svg"
                   alt="logo"
                   width={140}
                   height={30}
