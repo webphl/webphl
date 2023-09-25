f5af0de09b63529447ee03dbb9385baf058a5f28
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-09-25 15:45:53 +0200

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="Page Présentation"
+        pageName="Page de Présentation"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
       <AboutSectionOne />
