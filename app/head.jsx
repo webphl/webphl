2678a2a65d022dcbb15a09452cd1d7f792d65090
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-09-28 13:24:36 +0200

    Add files via upload

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Agence WebPhL - Créations de Sites Web, CRM, ERP, Mails, Sécurité</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="WebPhL - Agence Web depuis 2015" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
