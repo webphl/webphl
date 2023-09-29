f35eadcd4349bb7f9e7ae49be8eaf77806c8f9e9
WebPhL.com <102985753+webphl@users.noreply.github.com>
2023-09-29 21:39:41 +0200

    Update index.jsx

--- components/Video/index.jsx
+++ components/Video/index.jsx
@@ -53,7 +53,7 @@ const Video = () => {
         autoplay={true}
         start={true}
         isOpen={isOpen}
-        videoId="8gPzIKe92-M?t=2146&si=3s_QOxHFth3zGGq8"
+        videoId="xDFWLudn5WY"
         onClose={() => setOpen(false)}
       />
 
