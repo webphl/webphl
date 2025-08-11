4d325dd0f7f5d8f2da00477c35987489a079562e
WebPhL.com <102985753+webphl@users.noreply.github.com>
2025-08-11 18:03:35 +0200

    Update index.jsx

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
               <div className="mb-12 max-w-[360px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
                   <Image
-                    src="images/logo/logo-2.svg"
+                    src="images/logo/logo-webphl.png"
                     alt="logo"
                     className=""
                     width={140}
