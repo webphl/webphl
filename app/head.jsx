f6b0739416548d27fbc0d580819f1b854db0c97d
WebPhL.com <102985753+webphl@users.noreply.github.com>
2025-08-11 17:22:53 +0200

    Update head.jsx
    Modification du titre du site dans le Header

--- app/head.jsx
+++ app/head.jsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Agence WebPhL - Créations de Sites Web, CRM, ERP, Mails, Sécurité</title>
+      <title>WEBPHL SAS - Créations de Sites Web, CRM, ERP, Mails, Cybersécurité</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="WebPhL - Agence Web depuis 2015" />
       <link rel="icon" href="/images/favicon.ico" />
