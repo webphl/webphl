26dfbd49b1ce344cfd159cb42d1367c0b6ddaf69
WebPhL.com <102985753+webphl@users.noreply.github.com>
2025-08-11 17:25:04 +0200

    Update head.jsx

--- app/head.jsx
+++ app/head.jsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>WEBPHL SAS - Créations de Sites Web, CRM, ERP, Mails, Cybersécurité</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="WebPhL - Agence Web depuis 2015" />
+      <meta name="description" content="webphl.com since 2015" />
       <link rel="icon" href="/images/favicon.ico" />
     </>
   );
